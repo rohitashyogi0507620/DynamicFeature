ca9785d7f90ef0008f0de85bddda9bd29e198440
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-01-19 13:07:04 +0530

    Final Upload

--- app/src/main/java/com/onlineaavedan/dynamicfeature/MainActivity.java
+++ app/src/main/java/com/onlineaavedan/dynamicfeature/MainActivity.java
@@ -2,43 +2,101 @@ package com.onlineaavedan.dynamicfeature;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.play.core.splitinstall.SplitInstallManager;
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
 import com.google.android.play.core.splitinstall.SplitInstallRequest;
+import com.google.android.play.core.splitinstall.SplitInstallSessionState;
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
 import com.google.android.play.core.tasks.OnFailureListener;
 import com.google.android.play.core.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnaddfeature;
-    int resId;
+    Button btnaddfeature, btn_startactivity;
+
+    private int mySessionId;
+    private static final String TAG = "MainActivity";
+    private SplitInstallManager splitInstallManager;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnaddfeature = findViewById(R.id.btn_addfeature);
+        btn_startactivity = findViewById(R.id.btn_activity);
+
+        splitInstallManager = SplitInstallManagerFactory.create(getApplicationContext());
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Module Downloading ....");
         btnaddfeature.setOnClickListener(view -> {
+            progressDialog.show();
+            downloadDynamicModule();
+        });
 
-            SplitInstallManager splitInstallManager = SplitInstallManagerFactory.create(getApplicationContext());
-            SplitInstallRequest splitInstallRequest = SplitInstallRequest.newBuilder().addModule(getString(R.string.title_capturephotos)).build();
-            splitInstallManager.startInstall(splitInstallRequest)
-                    .addOnSuccessListener(new OnSuccessListener<Integer>() {
-                        @Override
-                        public void onSuccess(Integer integer) {
-                            Toast.makeText(getApplicationContext(), "Install SuccessFully", Toast.LENGTH_SHORT).show();
-                        }
+        btn_startactivity.setOnClickListener(V ->
+        {
+            Intent i = new Intent();
+            i.setClassName(BuildConfig.APPLICATION_ID, "com.onlineaavedan.capturephotos.CaptureActivity");
+            i.putExtra("ExtraInt", 3); // Test intent for Dynamic feature
+            startActivity(i);
 
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(Exception e) {
-                    Toast.makeText(getApplicationContext(), "Failed .." + e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void downloadDynamicModule() {
+
+
+        SplitInstallRequest request = SplitInstallRequest.newBuilder().addModule(getString(R.string.title_capturephotos)).build();
+
+        SplitInstallStateUpdatedListener listener = new SplitInstallStateUpdatedListener() {
+            @Override
+            public void onStateUpdate(SplitInstallSessionState splitInstallSessionState) {
+                if (splitInstallSessionState.sessionId() == mySessionId) {
+                    switch (splitInstallSessionState.status()) {
+                        case SplitInstallSessionStatus.DOWNLOADING:
+                            Toast.makeText(MainActivity.this, "Dynamic Module downloading", Toast.LENGTH_SHORT).show();
+                            // Update progress bar.
+                            break;
+                        case SplitInstallSessionStatus.INSTALLED:
+                            Log.d(TAG, "Dynamic Module downloaded");
+                            Toast.makeText(MainActivity.this, "Dynamic Module downloaded", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Session Not Created", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
+        };
+
+        splitInstallManager.registerListener(listener);
+
+        splitInstallManager.startInstall(request).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(MainActivity.this, "Failed to Install " + e, Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "ExceptionV: " + e);
+            }
+        }).addOnSuccessListener(new OnSuccessListener<Integer>() {
+            @Override
+            public void onSuccess(Integer sessionId) {
+                mySessionId = sessionId;
+                progressDialog.dismiss();
+                Toast.makeText(MainActivity.this, "Success" + sessionId, Toast.LENGTH_SHORT).show();
+                Intent i = new Intent();
+                i.setClassName(BuildConfig.APPLICATION_ID, "com.onlineaavedan.capturephotos.CaptureActivity");
+                i.putExtra("ExtraInt", 3); // Test intent for Dynamic feature
+                startActivity(i);
+            }
         });
     }
 }
